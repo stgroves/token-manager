51702ea457542ac6ff4491475eb6ec5afaae0b6b
STGroves <18527147+STGroves@users.noreply.github.com>
2025-05-20 23:43:52 +0100

    Added debug for key check

--- generate-jwt.js
+++ generate-jwt.js
@@ -3,7 +3,10 @@ const jwt = require('jsonwebtoken');
 const APP_ID = '1301208'; // Replace with your GitHub App ID
 const PRIVATE_KEY = process.env.PEM;
 
-module.exports = function generateJWT() {
+if (!PRIVATE_KEY)
+    throw new Error("Missing GitHub App Private Key");
+
+function generateJWT() {
     const payload = {
         iat: Math.floor(Date.now() / 1000), // Issued at time
         exp: Math.floor(Date.now() / 1000) + 600, // Expires in 10 minutes
@@ -12,3 +15,5 @@ module.exports = function generateJWT() {
 
     return jwt.sign(payload, PRIVATE_KEY, { algorithm: "RS256" });
 }
+
+module.exports = generateJWT;
