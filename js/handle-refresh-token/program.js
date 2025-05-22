b5dbaf9a8f3cf08252c11c2a914fa45151467566
STGroves <18527147+STGroves@users.noreply.github.com>
2025-05-22 01:46:30 +0100

    Changed connection.

--- js/handle-refresh-token/program.js
+++ js/handle-refresh-token/program.js
@@ -24,7 +24,7 @@ export default async function () {
                 }
             );
 
-            console.log(response.refresh_token);
+            console.log(response);
         }
 
         console.log("Refreshing OAuth token...");
