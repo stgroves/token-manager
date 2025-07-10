2fb994dc9382db6a63dd6bd6e1f228810deb8534
Stephanie Groves <18527147+stgroves@users.noreply.github.com>
2025-07-10 17:04:39 +0100

    Delete js directory

--- js/read-repo-file.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-export default function () {
-    return fs.readFileSync(process.env.REPOS, 'utf-8')
-        .split('\n') // Split by new line
-        .map(repo => repo.trim().split('/').pop()) // Get Repo names
-        .filter(repo => repo); // Remove blank lines
-}
